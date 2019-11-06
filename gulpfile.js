4c1d14d2bbb3bb884ea5660c1d48ed8a01f2540c
Amay Kataria <amkatari@microsoft.com>
2019-11-06 16:33:23 -0600

    Integrate Nest. Gulp is still really badly broken

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var header = require('gulp-header');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
-var filter = require('gulp-filter');
 var pkg = require('./package.json');
 
 // Set the banner content
@@ -17,9 +16,15 @@ var banner = ['/*!\n',
   ''
 ].join('');
 
+gulp.task('hello', function(done) {
+  console.log('Hello World');
+  done();
+});
+
+
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
-  return gulp.src('scss/freelancer.scss')
+  gulp.watch('scss/freelancer.scss')
     .pipe(sass())
     .pipe(header(banner, {
       pkg: pkg
@@ -32,7 +37,7 @@ gulp.task('sass', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/freelancer.css')
+  gulp.watch('css/freelancer.css')
     .pipe(cleanCSS({
       compatibility: 'ie8'
     }))
@@ -45,71 +50,71 @@ gulp.task('minify-css', ['sass'], function() {
     }))
 });
 
-// Minify custom JS
-gulp.task('minify-js', function() {
-  return gulp.src('js/freelancer.js')
-    .pipe(uglify())
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('js'))
-    .pipe(browserSync.reload({
-      stream: true
-    }))
-});
+// // Minify custom JS
+// gulp.task('minify-js', function() {
+//   return gulp.src('js/freelancer.js')
+//     .pipe(uglify())
+//     .pipe(header(banner, {
+//       pkg: pkg
+//     }))
+//     .pipe(rename({
+//       suffix: '.min'
+//     }))
+//     .pipe(gulp.dest('js'))
+//     .pipe(browserSync.reload({
+//       stream: true
+//     }))
+// });
 
-// Copy vendor files from /node_modules into /vendor
-// NOTE: requires `npm install` before running!
-gulp.task('copy', function() {
-  gulp.src([
-      'node_modules/bootstrap/dist/**/*',
-      '!**/npm.js',
-      '!**/bootstrap-theme.*',
-      '!**/*.map'
-    ])
-    .pipe(gulp.dest('vendor/bootstrap'))
+// // Copy vendor files from /node_modules into /vendor
+// // NOTE: requires `npm install` before running!
+// gulp.task('copy', function() {
+//   gulp.src([
+//       'node_modules/bootstrap/dist/**/*',
+//       '!**/npm.js',
+//       '!**/bootstrap-theme.*',
+//       '!**/*.map'
+//     ])
+//     .pipe(gulp.dest('vendor/bootstrap'))
 
-  gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-    .pipe(gulp.dest('vendor/jquery'))
+//   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+//     .pipe(gulp.dest('vendor/jquery'))
 
-  gulp.src(['node_modules/jquery.easing/*.js'])
-    .pipe(gulp.dest('vendor/jquery-easing'))
+//   gulp.src(['node_modules/jquery.easing/*.js'])
+//     .pipe(gulp.dest('vendor/jquery-easing'))
 
-  gulp.src(['node_modules/magnific-popup/dist/*'])
-    .pipe(gulp.dest('vendor/magnific-popup'))
+//   gulp.src(['node_modules/magnific-popup/dist/*'])
+//     .pipe(gulp.dest('vendor/magnific-popup'))
 
-  gulp.src([
-      'node_modules/font-awesome/**',
-      '!node_modules/font-awesome/**/*.map',
-      '!node_modules/font-awesome/.npmignore',
-      '!node_modules/font-awesome/*.txt',
-      '!node_modules/font-awesome/*.md',
-      '!node_modules/font-awesome/*.json'
-    ])
-    .pipe(gulp.dest('vendor/font-awesome'))
-})
+//   gulp.src([
+//       'node_modules/font-awesome/**',
+//       '!node_modules/font-awesome/**/*.map',
+//       '!node_modules/font-awesome/.npmignore',
+//       '!node_modules/font-awesome/*.txt',
+//       '!node_modules/font-awesome/*.md',
+//       '!node_modules/font-awesome/*.json'
+//     ])
+//     .pipe(gulp.dest('vendor/font-awesome'))
+// })
 
-// Default task
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+// // Default task
+// gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
 
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: ''
-    },
-  })
-})
+// // Configure the browserSync task
+// gulp.task('browserSync', function() {
+//   browserSync.init({
+//     server: {
+//       baseDir: ''
+//     },
+//   })
+// })
 
-// Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
-  gulp.watch('scss/*.scss', ['sass']);
-  gulp.watch('css/*.css', ['minify-css']);
-  gulp.watch('js/*.js', ['minify-js']);
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('*.html', browserSync.reload);
-  gulp.watch('js/**/*.js', browserSync.reload);
-});
+// // Dev task with browserSync
+// gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
+//   gulp.watch('scss/*.scss', ['sass']);
+//   gulp.watch('css/*.css', ['minify-css']);
+//   gulp.watch('js/*.js', ['minify-js']);
+//   // Reloads the browser whenever HTML or JS files change
+//   gulp.watch('*.html', browserSync.reload);
+//   gulp.watch('js/**/*.js', browserSync.reload);
+// });
