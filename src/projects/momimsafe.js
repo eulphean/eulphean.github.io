5e0841b4a9ebbee49836ca617548ea1c8d70fdcf
Amay Kataria <amaykataria@Amays-MBP.attlocal.net>
2020-12-23 22:57:31 -0600

    Momimsafe

--- src/projects/momimsafe.js
+++ src/projects/momimsafe.js
@@ -3,7 +3,13 @@ import Radium from 'radium'
 import Header from '../components/Header.js'
 import { commonWorkStyles, cvStyles, padding } from '../components/CommonStyles.js'
 import CustomLink from '../components/CustomLink.js'
-import antidoteA from '../images/momimsafe/antidote2.jpg'
+import momsafe from '../images/momimsafe/momimsafe.jpg'
+import antidoteA from '../images/momimsafe/AntidoteA.jpg'
+import antidoteB from '../images/momimsafe/AntidoteB.jpg'
+import antidoteC from '../images/momimsafe/AntidoteC.jpg'
+import labourA from '../images/momimsafe/labourA.jpg'
+import labourB from '../images/momimsafe/labourB.jpg'
+
 
 const styles={
     container: {
@@ -22,12 +28,10 @@ const styles={
     }
 }
 
-const linkA=(<CustomLink to='https://momimsafe.live' inline={true}>momimsafe.live</CustomLink>);
-const body=" is an active live-stream of my home studio space, where I spend the majority of my time in this post-covid era. It was developed in response to the COVID-19 lockdown, with an urgent need in mind; to be visually and physically accessible to all my friends and family globally; especially my mom, who has been concerned about my safety and health during this time."
-const linkB=(<CustomLink to="https://momimsafe.live/#/printer" inline={true}>Virtual Printer</CustomLink>); 
-const bodyB=" is an interactive simulation of the thermal printer placed in my home studio, which prints messages sent with momimsafe.live in real-time. This simulacra allows the participants to explore the entire stream of messages received during this work."; 
-const title=(<span style={cvStyles.title}>(covid-19) Antidote</span>)
-const bodyC=" is a limited edition artist book that aggregates portions of messages received with momimsafe.live. It blurs the line between digital and physical tactility of information to emphasize the courage derived from the kindness of loved ones through a warm technological gesture."; 
+
+const body=<span><CustomLink to='https://momimsafe.live' inline={true}>momimsafe.live</CustomLink> is an interactive website that ran as an active live-stream of my home studio space in Chicago from April - August, 2020, where I spent the majority of my time during the Coronavirus pandemic. It was intially developed in response to the COVID-19 lockdown, with an urgent need in mind; to be visually and physically accessible to all my global friends and family. An alternate mode of intimacy allowed one to also send a message from the website, which got printed in real-time in my space with a thermal printer. The work is now in an archived state, where a single video is playing on loop.</span>
+const bodyB=<span><CustomLink to="https://momimsafe.live/#/printer" inline={true}>Virtual Printer</CustomLink> is an interactive simulation of the thermal printer placed in my home studio. This simulacra allows participants to explore the entire database of messages received during this work.</span>
+const bodyC=<span><span style={cvStyles.title}>Labour of Love</span> (top) is an on-going series that pins these messages as a reminder of empathy, resilience, and strength in a time of uncertainty and isolation.<span style={cvStyles.title}>(covid-19) Antidote</span> (bottom) is a limited edition artist book that reinterprets these messages with medicinal values.</span>
 
 class momimsafe extends React.Component {
   constructor(props) {
@@ -44,24 +48,34 @@ class momimsafe extends React.Component {
             <Header title="MOMIMSAFE" />
             <div style={styles.content}>
               <div style={commonWorkStyles.body}>
-                {linkA}
                 {body}
               </div>
               <div style={commonWorkStyles.imgContainer}>
                 <iframe title={'momimsafe.live'} style={commonWorkStyles.webFrame} src={'https://momimsafe.live'}></iframe>
               </div>
               <div style={[commonWorkStyles.body, styles.additionPadding]}>
-                {linkB}
                 {bodyB}
               </div>
               <div style={commonWorkStyles.imgContainer}>
                 <iframe title={'virtual printer'} style={commonWorkStyles.webFrame} src={'https://momimsafe.live/#/printer'}></iframe>
               </div>
+              <img style={oneImgStyle} alt={'momimsafe'} src={momsafe}/>
+              <div style={commonWorkStyles.imgContainer}>
+                <div style={commonWorkStyles.multiImageContainer}>
+                  <img style={commonWorkStyles.twoImages} alt={'labourA'} src={labourA}/>
+                  <img style={commonWorkStyles.twoImages} alt={'labourB'} src={labourB}/>
+                </div>
+              </div>
               <div style={[commonWorkStyles.body, styles.additionPadding]}>
-                {title}
                 {bodyC}
               </div>
-              <img style={oneImgStyle} alt={'AntidoteA'} src={antidoteA}/>
+              <div style={commonWorkStyles.imgContainer}>
+                <div style={commonWorkStyles.multiImageContainer}>
+                    <img style={commonWorkStyles.threeImages} alt={'AntidoteA'} src={antidoteA}/>
+                    <img style={commonWorkStyles.threeImages} alt={'AntidoteB'} src={antidoteB}/>
+                    <img style={commonWorkStyles.threeImages} alt={'AntidoteC'} src={antidoteC}/>
+                </div>
+              </div>
             </div>
         </div>
     );
