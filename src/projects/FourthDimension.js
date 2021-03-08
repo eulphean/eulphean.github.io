6ce07a25635876e0d126898e59fa5e5256590e81
Amay Kataria <amaykataria@Amays-MacBook-Pro.local>
2021-03-08 15:25:10 -0600

    Add Forth Dimension page

--- src/projects/FourthDimension.js
+++ src/projects/FourthDimension.js
@@ -24,7 +24,7 @@ const styles={
     }
 }
 
-const body=<span></span>
+const body=<span>As part of <CustomLink to='https://deconfine.org/amay-kataria/' inline={true}>Deconfine Festival</CustomLink> 2020, I was invited to give a two-day online seminar based on my research and interests into the cultural history of the Fourth Dimension. The title of the workshop along with all the historical material was borrowed from the book "The Fourth Dimension and Non-Euclidean Geometry in Modern Art" by Linda Dalrymple Henderson. On the first day, I introduced the participants to Augmented Reality with SparkAR as one of the tools to mediate higher dimensions. Along with the historical development of this idea, we conducted a visual and spatial deconstruction of a tesseract using an <CustomLink to='https://amaykataria.com/Tesseract/' inline={true}>interactive</CustomLink> tool that I developed to better the understand the fourth dimension. The second day was discursive and led by the participants. Every one of them brought a previous project, thoughts, questions, and insights about the fourth dimension, which were expanded and discussed collaboratively. The seminar was mediated by Zoom and was attended by eight selected students across art & design programs in the most reputed colleges in India. You can find a short summary of the seminar below.</span>
 
 class KidGames extends React.Component {
   constructor(props) {
