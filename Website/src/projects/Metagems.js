2ee385bdc85e0de6e938b530c188773fcbd58a31
Amay Kataria <amay.kataria@gmail.com>
2023-12-21 16:30:38 -0600

    Add metagems to the repo

--- Website/src/projects/Metagems.js
+++ Website/src/projects/Metagems.js
@@ -3,12 +3,20 @@ import Radium from 'radium'
 import Header from '../components/Header.js'
 import CustomLink from '../components/CustomLink.js'
 import {commonWorkStyles} from '../components/CommonStyles.js'
-import one from '../images/WhaleWatcher/1.jpg'
-import two from '../images/WhaleWatcher/2.jpg'
-import three from '../images/WhaleWatcher/3.jpg'
-import four from '../images/WhaleWatcher/4.jpg'
-import five from '../images/WhaleWatcher/5.mp4'
-import six from '../images/WhaleWatcher/6.jpg'
+import one from '../images/Metagems/1.jpg'
+import two from '../images/Metagems/2.jpg'
+import three from '../images/Metagems/3.jpg'
+import four from '../images/Metagems/4.jpg'
+import five from '../images/Metagems/5.jpg'
+import six from '../images/Metagems/6.jpg'
+import seven from '../images/Metagems/7.jpg'
+import eight from '../images/Metagems/8.jpg'
+import nine from '../images/Metagems/9.jpg'
+import ten from '../images/Metagems/10.jpg'
+import eleven from '../images/Metagems/11.jpg'
+import twelve from '../images/Metagems/12.jpg'
+import thirteen from '../images/Metagems/13.jpg'
+import fourteen from '../images/Metagems/14.jpg'
 
 const styles={
     container: {
@@ -24,12 +32,10 @@ const styles={
 }
 
 
-const bodyA=<span><CustomLink to='https://visualizer-hackathon-75m2ad2pn-eulphean.vercel.app/' inline={true}>Whale Watcher</CustomLink> is an interactive data visualization project developed during the <CustomLink to='https://ethchicago.xyz/#/winners' inline={true}>ETHChicago 2023 Hackathon</CustomLink>. It won the first prize in the Featured Artists and Creatives category. Whale Watcher provides NFT creators with a bird's eye view of every collector who has ever owned their collection. Through this tool, creators can see their legacy through the lens of their community, in real time. Currently, Whale Watcher is in further development for a production release.</span>
-const bodyB=<span>In Whale Watcher, each whale represents a wallet address that holds an asset in the collection. The size of the whale is relative to the number of NFTs they possess. This allows users to quickly identify loyal hodlers and the biggest collectors.</span>
-const bodyC=<span></span>
-const bodyD=<span>Collaborators: <CustomLink to='https://www.maxknivets.com/' inline={true}>Max Knivets</CustomLink>, <CustomLink to='https://blairmckee.com/' inline={true}>Blair McKee</CustomLink></span>
+const bodyA=<span>Metagems is a new sculpture series that explores the captivating world of Chicago’s hidden gems. Inspired by the endangered flora and fauna of the city, the flower sculptures in this collection pay homage to the delicate beauty that surrounds us. Additionally, the lion sculpture is a tribute to the iconic historical lions that symbolize the resilience and grandeur of Chicago's past. These meticulously crafted handheld sculptures are a precursor for the large-scale sculptures that await in this series. Brace yourself for the intricate beauty of Metagems, where artistry and the essence of the city will intertwine.</span>
+const bodyB=<span>Collaborator: <CustomLink to='https://www.behance.net/braxtonchassag?locale=en_US&' inline={true}>Braxton Myles</CustomLink></span>
 
-class WhaleWatcher extends React.Component {
+class Metagems extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -47,27 +53,40 @@ class WhaleWatcher extends React.Component {
                 {bodyA}
                 <br/><br/>
                 {bodyB}
-                <br/><br/>
-                {bodyD}
               </div>
               <div style={commonWorkStyles.imgContainer}>
                 <div style={commonWorkStyles.multiImageContainer}>
-                    <img style={commonWorkStyles.twoImages} alt={'Two'} src={two}/>
-                    <img style={commonWorkStyles.twoImages} alt={'Three'} src={three}/>
+                    <img style={commonWorkStyles.threeImages} alt={'One'} src={one}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Two'} src={two}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Three'} src={three}/>
                 </div>
               </div>
               <div style={commonWorkStyles.imgContainer}>
-                  <img style={commonWorkStyles.oneImage} alt={'One'} src={one}/>
-              </div>
-              <div style={commonWorkStyles.imgContainer}>
                 <div style={commonWorkStyles.multiImageContainer}>
-                    <img style={commonWorkStyles.threeImages} alt={'Four'} src={four}/>
-                    <video autoPlay loop muted style={commonWorkStyles.threeImages} alt={'Five'} src={five}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Five'} src={five}/>
                     <img style={commonWorkStyles.threeImages} alt={'Six'} src={six}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Four'} src={four}/>
                 </div>
               </div>
-              <div style={containerStyle}>
-                <iframe title={'Whale Watcher'} style={commonWorkStyles.vimeoFrame} src="https://player.vimeo.com/video/872193906" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen></iframe>
+              <div style={commonWorkStyles.imgContainer}>
+                <div style={commonWorkStyles.multiImageContainer}>
+                    <img style={commonWorkStyles.threeImages} alt={'Seven'} src={seven}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Eight'} src={eight}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Nine'} src={nine}/>
+                </div>
+              </div>
+              <div style={commonWorkStyles.imgContainer}>
+                <div style={commonWorkStyles.multiImageContainer}>
+                    <img style={commonWorkStyles.twoImages} alt={'Ten'} src={ten}/>
+                    <img style={commonWorkStyles.twoImages} alt={'Eleven'} src={eleven}/>
+                </div>
+              </div>
+              <div style={commonWorkStyles.imgContainer}>
+                <div style={commonWorkStyles.multiImageContainer}>
+                    <img style={commonWorkStyles.threeImages} alt={'Twelve'} src={twelve}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Thirteen'} src={thirteen}/>
+                    <img style={commonWorkStyles.threeImages} alt={'Fourteen'} src={fourteen}/>
+                </div>
               </div>
             </div>
         </div>
@@ -75,4 +94,4 @@ class WhaleWatcher extends React.Component {
   }
 }
 
-export default Radium(WhaleWatcher);
+export default Radium(Metagems);
