ee003cd4165b2a62385753e12864fd06af07978c
Amay Kataria <amay.kataria@gmail.com>
2024-12-30 09:52:41 -0600

    Adding another fix for overflow

--- 3.0/components/Layout.tsx
+++ 3.0/components/Layout.tsx
@@ -16,7 +16,7 @@ export default function Layout({children,home}: LayoutProps) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
-      <main className='h-screen'>{children}</main>
+      <main className='h-screen overflow-hidden'>{children}</main>
     </div>
   );
 }
