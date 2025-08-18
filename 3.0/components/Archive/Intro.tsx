68db21cca3667b13430077521fb750edb2c13a6d
Amay Kataria <to.amaykataria@gmail.com>
2025-08-18 13:19:24 -0500

    Bug fix

--- 3.0/components/Archive/Intro.tsx
+++ 3.0/components/Archive/Intro.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react"
-import CustomButton from "./CustomButton";
+import CustomButton from "../CustomButton";
 import SplineScene, { SceneType } from "./SplineScene";
 import Loader from './Loader'
 import { useGSAP } from "@gsap/react";
